41cf168ab3f0cc6ecdb3217518e34d08d3fefa9f
Andrei Proskurin <San4taex@yandex.by>
2020-05-19 17:51:07 +0300

    Add project files.

---
+++ Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tests
+{
+    public static class Utils
+    {
+        public static string GetRandomString(int length)
+        {
+            var random = new Random();
+            if (length == 0)
+            {
+                length = random.Next(65);
+            }
+            var chars = "abcdefghijklmnopqrstuvwxyz0123456789 ";
+            var stringChars = new char[length];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new String(stringChars);
+        }
+    }
+}
